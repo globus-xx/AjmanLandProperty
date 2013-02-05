47b6184d0f5733a2c5d815523d16329801756f90
m <m@m-VAIO>
2013-02-05 17:49:34 +0400

    Dear Omar,
    i have finished the web service 
    
    notice : the link you should use is this:
    localhost/AjmanLandProperty/index.php/LPDWebService/?token=ENCRYPTEDCODE
    
    now the encrypted code is in this form 
    anyname|anydate|landid 
    
    but this form should encrypted  by our function 
    Thanks

---
+++ protected/controllers/LPDWebServiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+class LPDWebServiceController extends Controller
+{
+    
+       
+        
+	public function actionIndex()
+	{                        
+             $ky = '!GIS_LaPD@2013!&&!GIS_LaPD@2013!'; // 32 * 8 = 256 bit key
+             $iv = '!LaPD_GIS@2013!&&!LaPD_GIS@2013!'; // 32 * 8 = 256 bit iv
+        
+             $codetodecrypt=$_GET['token'];
+                                          
+             $resultstring = $this->decryptRJ256($ky,$iv,$codetodecrypt);
+             $resultstring = explode("|",$resultstring);
+             
+             $landid=$resultstring[2];
+             
+            
+             
+              
+             ?>
+<script type="text/javascript" src="/AjmanLandProperty/js/jquery-1.8.1.min.js"></script> 
+             <script>
+                   
+             
+               var landid ='<?php echo $landid; ?>'
+            $.ajax({ 
+                                type: "POST",
+				url:'http://localhost/AjmanLandProperty/index.php/CustomerService/Search', 
+				data: "action=search&string="+landid+"&returnType=ws",
+                                async:false,
+				success: function(data) { 
+					var Results = JSON.parse(data); 	
+					console.log(Results);                                        
+                                        document.write(data);                               
+				}
+                        });  
+                                                     
+             </script> 
+               
+              
+             
+<? 
+  
+}
+
+	
+        
+       
+
+       public  function decryptRJ256($key,$iv,$string_to_decrypt)
+        {
+        $string_to_decrypt = base64_decode($string_to_decrypt);
+        $rtn = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $string_to_decrypt, MCRYPT_MODE_CBC, $iv);
+        $rtn = rtrim($rtn, "\0\4");
+        return($rtn);
+        }
+
+
+       public function encryptRJ256($key,$iv,$string_to_encrypt)
+        {
+        $rtn = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $string_to_encrypt, MCRYPT_MODE_CBC, $iv);
+        $rtn = base64_encode($rtn);
+        return($rtn);
+        }
+        
+        
+        
+        
+        // Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
