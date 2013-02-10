f7d7b0fffb40b896c50d9469154b547ebb172cb1
hasan <hasan@hasan-PC>
2013-02-10 14:40:24 +0400

    i have updated the getdata view in lpdwebservice folder there is small bug i think this is the problem please pull your project
    Thanks ,
    Emad

--- protected/views/lpdwebservice/getdata.php
+++ protected/views/lpdwebservice/getdata.php
@@ -4,15 +4,17 @@
                    
              
                var landid ='<?php echo $landid; ?>'
+               var base='<?php echo Yii::app()->request->baseUrl;?>';
             $.ajax({ 
                                 type: "POST",
-				url:'http://localhost/AjmanLandProperty/index.php/CustomerService/Search', 
+				url:base + "/index.php/CustomerService/Search", 
 				data: "action=search&string="+landid+"&returnType=ws",
                                 async:false,
 				success: function(data) { 
 					var Results = JSON.parse(data); 	
-					console.log(Results);                                        
-                                        document.write(data);                               
+					console.log(Results);
+
+                                                             
 				}
                         });  
                                                      
